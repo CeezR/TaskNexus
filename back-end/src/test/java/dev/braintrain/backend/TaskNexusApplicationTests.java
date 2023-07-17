94eb0708863db6f0c3d8b1c661251b796e7995a8
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-17 09:23:03 +0200

    feat: springInitializer

---
+++ back-end/src/test/java/dev/braintrain/backend/TaskNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.braintrain.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
