fa0f46c4fb7017a1dff5d044d0962507ff10bbc4
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-18 08:54:49 +0200

    feat: created job(id, name) entity and added data.sql file to populate h2 database

---
+++ back-end/src/main/java/dev/braintrain/backend/job/Job.java
@@ -0,0 +1,31 @@
+package dev.braintrain.backend.job;
+
+import javax.persistence.*;
+
+@Entity
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "job_name", nullable = false)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
