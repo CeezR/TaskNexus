fedda23bd746e453b333aa1707b5a6a6637b0ce8
Luke Williams <luke.williams@appliedtechnology.se>
2023-07-18 09:18:22 +0200

    test: implemented test for JobService

---
+++ back-end/src/main/java/dev/braintrain/backend/job/JobService.java
@@ -0,0 +1,21 @@
+package dev.braintrain.backend.job;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+
+    private final JobRepository repo;
+
+    @Autowired
+    public JobService(JobRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Job> findAll() {
+        return null;
+    }
+}
---
+++ back-end/src/test/java/dev/braintrain/backend/job/JobServiceTest.java
@@ -0,0 +1,25 @@
+package dev.braintrain.backend.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JobServiceTest {
+
+    @Autowired
+    JobService service;
+
+    @Test
+    void findAllJobs() {
+        List<Job> jobs = service.findAll();
+        assertThat(jobs).isNotNull();
+        assertThat(jobs.size()).isEqualTo(3);
+    }
+
+}
