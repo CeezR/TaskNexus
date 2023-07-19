6393f4dbfb6f434255e9f84451ca04788895644f
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-19 14:03:11 +0200

    fix: removed unused comment

--- front-end/app/listings/jobs/[jobId]/page.tsx
+++ front-end/app/listings/jobs/[jobId]/page.tsx
@@ -1,41 +1,42 @@
 "use client"
 import React, { useState, useEffect } from 'react'
 
-type Props ={
-    params: {
-        jobId: string;
-    }
+type Props = {
+  params: {
+    jobId: string;
+  }
 }
 
 const page = (props: Props) => {
-    const [job, setJob] = useState<Job>();
+  const [job, setJob] = useState<Job>();
 
-    // useEffect(() => {
-    //     getJob();
-    //   }, []);
+  useEffect(() => {
+    getJob();
+  }, []);
 
-    const getJob = async () => {
-        try {
-          const response = await fetch(
-            `https://tasknexus.azurewebsites.net/api/jobs/${props.params.jobId}`
-          );
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          const data: Job = await response.json();
-          setJob(data);
-          return data;
-        } catch (error) {
-          console.error("Error fetching data:", error);
-          return null;
-        }
-      };
-    
+  const getJob = async () => {
+    try {
+      const response = await fetch(
+        `https://tasknexus.azurewebsites.net/api/jobs/${props.params.jobId}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data: Job = await response.json();
+      console.log(data);
+      setJob(data);
+      return data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return null;
+    }
+  };
+
   return (
-  <section>
-    <h1>Job</h1>
-    <p>{job?.id}</p>
-  </section>
+    <section>
+      <h1>Job</h1>
+      <p>{job?.id}</p>
+    </section>
   )
 }
 
