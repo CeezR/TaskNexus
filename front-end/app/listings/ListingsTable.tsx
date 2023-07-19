82fd36d93dc222d462d20247ce7600fa0ecfc702
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-19 05:26:14 +0200

    fix: table no longer overflow on small screens

--- front-end/app/listings/ListingsTable.tsx
+++ front-end/app/listings/ListingsTable.tsx
@@ -24,7 +24,7 @@ const rows = [
 export default function ListingsTable({jobs} : BasicTableProp) {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Name</TableCell>
