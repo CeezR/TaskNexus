f658de794121032c1547769bbdd03065ec4e4ee2
Atufa Shabnam <atufa.shabnam@appliedtechnology.se>
2023-07-19 13:45:52 +0200

    feat: created functionality to get jobById

--- front-end/app/listings/jobs/[jobId]/page.tsx
+++ front-end/app/listings/jobs/[jobId]/page.tsx
@@ -1,9 +1,41 @@
 "use client"
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const page = () => {
+type Props ={
+    params: {
+        jobId: string;
+    }
+}
+
+const page = (props: Props) => {
+    const [job, setJob] = useState<Job>();
+
+    // useEffect(() => {
+    //     getJob();
+    //   }, []);
+
+    const getJob = async () => {
+        try {
+          const response = await fetch(
+            `https://tasknexus.azurewebsites.net/api/jobs/${props.params.jobId}`
+          );
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data: Job = await response.json();
+          setJob(data);
+          return data;
+        } catch (error) {
+          console.error("Error fetching data:", error);
+          return null;
+        }
+      };
+    
   return (
-    <div>page</div>
+  <section>
+    <h1>Job</h1>
+    <p>{job?.id}</p>
+  </section>
   )
 }
 
