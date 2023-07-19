5a46de4753b124b3ff7990e6b269fa06ec6041b8
Atufa Shabnam <atufa.shabnam@appliedtechnology.se>
2023-07-19 16:15:21 +0200

    feat: implementing update logic

--- front-end/app/listings/jobs/[jobId]/page.tsx
+++ front-end/app/listings/jobs/[jobId]/page.tsx
@@ -1,10 +1,13 @@
 "use client";
-import { Button } from "@mui/material";
+import { Box, Button, Modal, TextField, Typography } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import Stack from "@mui/material/Stack";
 import { useRouter } from "next/navigation";
+import { Formik } from "formik";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Jobs from "../page";
 
 type Props = {
   params: {
@@ -12,13 +15,55 @@ type Props = {
   };
 };
 
+const style = {
+  position: "absolute" as const,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+
+
 const page = (props: Props) => {
   const [job, setJob] = useState<Job>();
   const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleFormSubmit = (values: InitialValues): void => {
+      alert(JSON.stringify(values, undefined, 2));
+      handleClose();
+      editJob(values);
+  };
 
   useEffect(() => {
     getJob();
   }, []);
+
+
+  interface InitialValues {
+    name: string;
+    // lastName: string;
+    // email: string;
+    // contact: string;
+    // address1: string;
+    // address2: string;
+  }
+
+  const initialValues: InitialValues = {
+    name: job?.name,
+    // lastName: "",
+    // email: "",
+    // contact: "",
+    // address1: "",
+    // address2: "",
+  };
 
   const getJob = async () => {
     try {
@@ -47,15 +92,34 @@ const page = (props: Props) => {
     }
   };
 
+  const editJob = async (requestBody: InitialValues) => {
+    const editedJob: Job = {
+      id: job?.id,
+      name: requestBody.name
+    }
+    console.log(job.id);
+  
+    const response = await fetch(`http://localhost:8080/api/jobs/${job?.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        
+        body: JSON.stringify(editedJob),
+    });
+    if (!response.ok) {
+        throw new Error("Failed to add job");
+    }
+  };
+
   const handleDelete = async () => {
     await deleteJob();
     router.push('/listings/jobs');
 
   }
+  const isNonMobile = useMediaQuery("(min-width:600px)");
 
-  const handleEdit = () => {
-    
-  }
+  
 
   return (
     <section>
@@ -65,10 +129,70 @@ const page = (props: Props) => {
         <Button onClick={handleDelete} variant="outlined" color="error" startIcon={<DeleteIcon />}>
           Delete
         </Button>
-        <Button onClick={handleEdit} variant="outlined" color="warning" startIcon={<EditIcon />}>
+        <Button onClick={handleOpen} variant="outlined" color="warning" startIcon={<EditIcon />}>
           Edit
         </Button>
       </Stack>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Employee Details
+          </Typography>
+          <Box>
+            <Formik
+              onSubmit={handleFormSubmit}
+              initialValues={initialValues}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  <Box
+                    display="grid"
+                    gap="30px"
+                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                    sx={{
+                      "& > div": {
+                        gridColumn: isNonMobile ? undefined : "span 4",
+                      },
+                    }}
+                  >
+                    <TextField
+                      fullWidth
+                      variant="filled"
+                      type="text"
+                      label="Name"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.name}
+                      name="name"
+                      error={!!touched.name && !!errors.name}
+                      helperText={touched.name && errors.name}
+                      sx={{ gridColumn: "span 4" }}
+                    />
+
+                  </Box>
+                  <Box display="flex" justifyContent="end" mt="20px">
+                    <Button type="submit" color="secondary" variant="contained">
+                      Edit 
+                    </Button>
+                  </Box>
+                </form>
+              )}
+            </Formik>
+          </Box>
+        </Box>
+      </Modal>
     </section>
   );
 };
