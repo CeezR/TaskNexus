abac0212550c4ca66170d105ed5d8e850c87ec51
Luke Williams <luke.williams@appliedtechnology.se>
2023-07-19 15:21:14 +0200

    feat: delete functionality implemented

--- front-end/app/listings/jobs/[jobId]/page.tsx
+++ front-end/app/listings/jobs/[jobId]/page.tsx
@@ -1,11 +1,15 @@
-"use client"
-import React, { useState, useEffect } from 'react'
+"use client";
+import { Button } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import Stack from "@mui/material/Stack";
 
 type Props = {
   params: {
     jobId: string;
-  }
-}
+  };
+};
 
 const page = (props: Props) => {
   const [job, setJob] = useState<Job>();
@@ -23,7 +27,6 @@ const page = (props: Props) => {
         throw new Error("Network response was not ok");
       }
       const data: Job = await response.json();
-      console.log(data);
       setJob(data);
       return data;
     } catch (error) {
@@ -32,12 +35,38 @@ const page = (props: Props) => {
     }
   };
 
+  const deleteJob = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/jobs/${job?.id}`, {
+        method: "DELETE",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDelete = () => {
+    deleteJob();
+  }
+
+  const handleEdit = () => {
+    
+  }
+
   return (
     <section>
       <h1>Job</h1>
-      <p>{job?.id}</p>
+      <h2>{job?.name}</h2>
+      <Stack direction="row" spacing={2}>
+        <Button onClick={handleDelete} variant="outlined" color="error" startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+        <Button onClick={handleEdit} variant="outlined" color="warning" startIcon={<EditIcon />}>
+          Edit
+        </Button>
+      </Stack>
     </section>
-  )
-}
+  );
+};
 
-export default page
+export default page;
