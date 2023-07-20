6a8c76dbff6de2cd5d22ecb852e0c6c0c4b71ed3
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-20 11:13:23 +0200

    test: getAll mapping CompanyController test implemented

---
+++ back-end/src/main/java/dev/braintrain/backend/company/CompanyResponseDTO.java
@@ -0,0 +1,2 @@
+package dev.braintrain.backend.company;public record CompanyResponseDTO() {
+}
---
+++ back-end/src/test/java/dev/braintrain/backend/company/CompanyControllerTest.java
@@ -0,0 +1,35 @@
+package dev.braintrain.backend.company;
+
+import dev.braintrain.backend.job.JobResponseDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+class CompanyControllerTest {
+    @Value("${server.port}")
+    private int port;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Test
+    void getAll() {
+        String uri = "http://localhost:%s/api/companies".formatted(port);
+        ResponseEntity<CompanyResponseDTO> exchange = restTemplate.exchange(uri, HttpMethod.GET, HttpEntity.EMPTY, CompanyResponseDTO.class);
+        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(exchange.hasBody()).isTrue();
+        assertThat(exchange.getBody().companyList()).isNotNull();
+    }
+}
