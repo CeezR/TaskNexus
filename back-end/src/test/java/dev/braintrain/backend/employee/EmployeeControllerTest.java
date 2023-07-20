da7807f9194f03f577e63aa3b3c94dff548ede3a
Luke Williams <luke.williams@appliedtechnology.se>
2023-07-20 10:56:12 +0200

    test: EmployeeControllerTest implemented

---
+++ back-end/src/test/java/dev/braintrain/backend/employee/EmployeeControllerTest.java
@@ -0,0 +1,37 @@
+package dev.braintrain.backend.employee;
+
+import dev.braintrain.backend.job.JobResponseDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+class EmployeeControllerTest {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Test
+    void getEmployeeMappingShouldReturnEmployeeList() {
+        String uri = "http://localhost:%s/api/employees".formatted(port);
+        ResponseEntity<EmployeeResponseDTO> exchange = restTemplate.exchange(uri, HttpMethod.GET, HttpEntity.EMPTY, EmployeeResponseDTO.class);
+        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(exchange.hasBody()).isTrue();
+        assertThat(exchange.getBody().employeeList()).isNotNull();
+    }
+
+}
