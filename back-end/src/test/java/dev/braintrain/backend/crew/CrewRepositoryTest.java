b79438bc221fa374646faccb41d52b4c4f845e14
Atufa Shabnam <atufa.shabnam@appliedtechnology.se>
2023-07-20 10:47:07 +0200

    test: created a test ReturnJobById

--- back-end/src/test/java/dev/braintrain/backend/crew/CrewRepositoryTest.java
+++ back-end/src/test/java/dev/braintrain/backend/crew/CrewRepositoryTest.java
@@ -1,7 +1,25 @@
 package dev.braintrain.backend.crew;
 
+import dev.braintrain.backend.job.Job;
+import dev.braintrain.backend.job.JobRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CrewRepositoryTest {
+
+    @Autowired
+    CrewRepository repository;
+
+    @Test
+    void shouldReturnJobById() {
+        Crew job = new Crew("Developer");
+        Crew createCrew = repository.save(crew);
+        assertThat(repository.findById(createCrew.getId())).isNotNull();
+    }
 
 }
