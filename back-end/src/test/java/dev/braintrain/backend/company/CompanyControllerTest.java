4a224146ce3cc7d547debc3f3752e877b374940c
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-20 11:20:58 +0200

    test: Comanpany postMapping test implemented

--- back-end/src/test/java/dev/braintrain/backend/company/CompanyControllerTest.java
+++ back-end/src/test/java/dev/braintrain/backend/company/CompanyControllerTest.java
@@ -1,6 +1,8 @@
 package dev.braintrain.backend.company;
 
+import dev.braintrain.backend.job.Job;
 import dev.braintrain.backend.job.JobResponseDTO;
+import dev.braintrain.backend.job.RequestJobDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,5 +33,16 @@ class CompanyControllerTest {
         assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(exchange.hasBody()).isTrue();
         assertThat(exchange.getBody().companyList()).isNotNull();
+    }
+
+    @Test
+    void shouldCreateJobForPostRequest() {
+        String uri = "http://localhost:%s/api/companies".formatted(port);
+        RequestCompanyDTO company = new RequestCompanyDTO("TestName");
+        ResponseEntity<Company> exchange = restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(company), Company.class);
+        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(exchange.hasBody()).isTrue();
+        assertThat(exchange.getHeaders().getLocation()).isNotNull();
+        assertThat(exchange.getBody()).isNotNull();
     }
 }
