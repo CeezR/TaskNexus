0e4051ac6467ba2ff2f9c35000bead6197b9e418
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-20 10:55:50 +0200

    fix: changed JPACompanyRepo type from Job to Company

--- back-end/src/main/java/dev/braintrain/backend/company/JPACompanyRepository.java
+++ back-end/src/main/java/dev/braintrain/backend/company/JPACompanyRepository.java
@@ -3,5 +3,5 @@ package dev.braintrain.backend.company;
 import dev.braintrain.backend.job.Job;
 import org.springframework.data.repository.CrudRepository;
 
-public interface JPACompanyRepository extends CrudRepository<Job, Long> {
+public interface JPACompanyRepository extends CrudRepository<Company, Long> {
 }
