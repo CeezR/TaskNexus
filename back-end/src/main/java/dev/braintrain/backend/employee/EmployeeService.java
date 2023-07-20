b9f2f385cc864ed2d01a07b83511b35e3c2979ba
Luke Williams <luke.williams@appliedtechnology.se>
2023-07-20 10:49:24 +0200

    feat: findAll in employeeService implemented

---
+++ back-end/src/main/java/dev/braintrain/backend/employee/EmployeeService.java
@@ -0,0 +1,22 @@
+package dev.braintrain.backend.employee;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository repository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Employee> findAll() {
+        return repository.findAll();
+    }
+}
