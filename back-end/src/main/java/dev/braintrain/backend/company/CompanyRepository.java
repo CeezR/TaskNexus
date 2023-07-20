8f00e62bcbd19367ddcaecc964f275d35716589b
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-20 10:57:11 +0200

    feat: implemented CRUD methods

--- back-end/src/main/java/dev/braintrain/backend/company/CompanyRepository.java
+++ back-end/src/main/java/dev/braintrain/backend/company/CompanyRepository.java
@@ -1,7 +1,12 @@
 package dev.braintrain.backend.company;
 
+import dev.braintrain.backend.job.Job;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class CompanyRepository {
@@ -10,5 +15,24 @@ public class CompanyRepository {
     @Autowired
     public CompanyRepository(JPACompanyRepository repo) {
         this.repo = repo;
+    }
+
+    public List<Company> findAll() {
+        return Streamable.of(repo.findAll()).toList();
+    }
+
+    public Company save(Company company) {
+        return repo.save(company);
+    }
+
+    public Optional<Company> findById(Long id) {
+        return repo.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        try {
+            repo.deleteById(id);
+        } catch (Exception ignored) {}
+
     }
 }
