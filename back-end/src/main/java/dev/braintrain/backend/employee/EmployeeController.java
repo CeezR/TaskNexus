a3ecc9888b03bf454fe13ae9a2ea2c762fd10320
Luke Williams <luke.williams@appliedtechnology.se>
2023-07-20 11:03:09 +0200

    feat: getAllEmployees implemented

--- back-end/src/main/java/dev/braintrain/backend/employee/EmployeeController.java
+++ back-end/src/main/java/dev/braintrain/backend/employee/EmployeeController.java
@@ -1,4 +1,28 @@
 package dev.braintrain.backend.employee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
+
+    private final EmployeeService service;
+
+    @Autowired
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<EmployeeResponseDTO> getAllEmployees() {
+        return ResponseEntity.ok().body(new EmployeeResponseDTO(service.findAll()));
+    }
 }
