5f2b849a0a149b17b871fd63c9579740717e66ce
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-07-20 11:07:17 +0200

    feat: setup Company Controller

--- back-end/src/main/java/dev/braintrain/backend/company/CompanyController.java
+++ back-end/src/main/java/dev/braintrain/backend/company/CompanyController.java
@@ -1,4 +1,23 @@
 package dev.braintrain.backend.company;
 
+import dev.braintrain.backend.job.JobResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/companies")
+@CrossOrigin(origins = "*")
 public class CompanyController {
+    private final CompanyService service;
+
+    @Autowired
+    public CompanyController(CompanyService service) {
+        this.service = service;
+    }
+
+
 }
