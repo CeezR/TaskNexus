ddd41a97581289d3f5e72353db3ab475521105be
Atufa Shabnam <atufa.shabnam@appliedtechnology.se>
2023-07-20 11:11:32 +0200

    test: added test to the controller to get the list of crew

--- back-end/src/test/java/dev/braintrain/backend/crew/CrewControllerTest.java
+++ back-end/src/test/java/dev/braintrain/backend/crew/CrewControllerTest.java
@@ -1,7 +1,37 @@
 package dev.braintrain.backend.crew;
 
+import dev.braintrain.backend.job.JobResponseDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 class CrewControllerTest {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Test
+    void getCrewMappingShouldReturnCrewList() {
+        String uri = "http://localhost:%s/api/crews".formatted(port);
+        ResponseEntity<CrewResponseDTO> exchange = restTemplate.exchange(uri, HttpMethod.GET, HttpEntity.EMPTY, CrewResponseDTO.class);
+        assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(exchange.hasBody()).isTrue();
+        assertThat(exchange.getBody().crewList()).isNotNull();
+    }
 
 }
