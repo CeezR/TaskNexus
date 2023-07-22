7dc2d164269a580500e2f96afb87637fcdd52010
Atufa Shabnam <atufa.shabnam@appliedtechnology.se>
2023-07-22 07:31:33 +0200

    style: changed the table type

--- front-end/app/listings/crews/ListingsTable.tsx
+++ front-end/app/listings/crews/ListingsTable.tsx
@@ -6,48 +6,78 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Link from 'next/link';
+import { DataGrid, GridColDef, GridRowParams } from "@mui/x-data-grid";
 import { ArrowForward } from '@mui/icons-material';
 import { useRouter } from 'next/navigation';
+import {
+  Box,
+  useTheme,
+  IconButton,
+} from "@mui/material";
+import { tokens } from "@/app/theme";
 
 
 type BasicTableProp = {
-  crews : Crew[] | undefined
+  crews: Crew[] | undefined
 }
 
 export default function ListingsTable({ crews }: BasicTableProp) {
   const router = useRouter();
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns: GridColDef[] = [
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+  ];
+  const handleRowClick = (params: GridRowParams) => {
 
+    const employeeId = params.row.id;
+
+
+    router.push(`/listings/crews/${employeeId}`);
+  };
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {crews?.length ? ( // Check if crews is not null and has elements
-            crews.map((crew, index) => (
-              <TableRow
-                key={index}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  <div className='tableListing__row' onClick={() => router.push(`/listings/crews/${crew.id}`)}>
-                    <p className='table-paragraph'>{crew.name}</p>
-                    <ArrowForward />
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-             
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    
+    <Box
+    m="40px 0 0 0"
+    height="75vh"
+    sx={{
+      "& .MuiDataGrid-root": {
+        border: "none",
+      },
+      "& .MuiDataGrid-cell": {
+        borderBottom: "none",
+      },
+      "& .name-column--cell": {
+        color: colors.greenAccent[300],
+      },
+      "& .MuiDataGrid-columnHeaders": {
+        backgroundColor: colors.blueAccent[700],
+        borderBottom: "none",
+      },
+      "& .MuiDataGrid-virtualScroller": {
+        backgroundColor: colors.primary[400],
+      },
+      "& .MuiDataGrid-footerContainer": {
+        borderTop: "none",
+        backgroundColor: colors.blueAccent[700],
+      },
+      "& .MuiCheckbox-root": {
+        color: `${colors.greenAccent[200]} !important`,
+      },
+    }}
+  >
+    <DataGrid
+        checkboxSelection
+        rows={crews as readonly any[]} 
+        columns={columns}
+        onRowClick={handleRowClick}
+      />
+  </Box>
   );
 }
+
