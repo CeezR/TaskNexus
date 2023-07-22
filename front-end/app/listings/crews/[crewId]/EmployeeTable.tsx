64e8292de5f19086023dae8a85dceefffee7e534
Atufa Shabnam <atufa.shabnam@appliedtechnology.se>
2023-07-22 07:39:26 +0200

    feat: changed the table type

--- front-end/app/listings/crews/[crewId]/EmployeeTable.tsx
+++ front-end/app/listings/crews/[crewId]/EmployeeTable.tsx
@@ -1,5 +1,12 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton } from "@mui/material";
-import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import React from "react";
+import {
+  Box,
+  useTheme,
+  IconButton,
+} from "@mui/material";
+import { ThemeProvider, styled } from "@mui/material/styles";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { tokens } from "@/app/theme";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 
@@ -8,76 +15,77 @@ type EmployeeTableProps = {
   onDeleteEmployee: (employeeId: number) => void;
 };
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#001f3f", // Navy Blue
-    },
-    secondary: {
-      main: "#0074d9", // Blue (header color)
-    },
-  },
-});
-
-const StyledTableContainer = styled(TableContainer)(({ theme }) => ({
-  marginBottom: theme.spacing(2),
-}));
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  padding: theme.spacing(2),
-  color: "#fff", // White text color
-}));
-
-const StyledTableHeaderCell = styled(TableCell)(({ theme }) => ({
-  backgroundColor: theme.palette.secondary.main,
-  color: "#fff", // White text color
-  fontWeight: "bold",
-  padding: theme.spacing(2),
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.primary.light,
-  },
-  '&:nth-of-type(even)': {
-    backgroundColor: theme.palette.primary.main,
-  },
-}));
-
 const EmployeeTable: React.FC<EmployeeTableProps> = ({ employees, onDeleteEmployee }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "actions", 
+      headerName: "Actions",
+      renderCell: ({ row }) => (
+        <IconButton onClick={() => onDeleteEmployee(row.id)} style={{ color: "red" }}>
+          <DeleteIcon />
+        </IconButton>
+      ),
+      sortable: false,
+      filterable: false,
+      width: 100,
+      disableColumnMenu: true,
+    },
+  ];
   return (
     <ThemeProvider theme={theme}>
-      <StyledTableContainer>
-        <Table aria-label="employee table">
-          <TableHead>
-            <StyledTableRow>
-              <StyledTableHeaderCell>ID</StyledTableHeaderCell>
-              <StyledTableHeaderCell>Name</StyledTableHeaderCell>
-              <StyledTableHeaderCell>Email</StyledTableHeaderCell>
-              <StyledTableHeaderCell>Phone Number</StyledTableHeaderCell>
-              <StyledTableHeaderCell>Action</StyledTableHeaderCell>
-            </StyledTableRow>
-          </TableHead>
-          <TableBody>
-            {employees.map((employee) => (
-              <StyledTableRow key={employee.id}>
-                <StyledTableCell>{employee.id}</StyledTableCell>
-                <StyledTableCell>{employee.name}</StyledTableCell>
-                <StyledTableCell>{employee.email}</StyledTableCell>
-                <StyledTableCell>{employee.phoneNumber}</StyledTableCell>
-                <StyledTableCell>
-                  <IconButton onClick={() => onDeleteEmployee(employee.id)} color="error">
-                    <DeleteIcon />
-                  </IconButton>
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </StyledTableContainer>
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={employees} columns={columns} />
+      </Box>
     </ThemeProvider>
   );
 };
 
 export default EmployeeTable;
-
