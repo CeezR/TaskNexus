55c9573b558014acf4922ea1f342e219e70daee1
Luke Williams <luke.williams@appliedtechnology.se>
2023-07-26 08:59:30 +0200

    fix: file field added back to Job

--- back-end/src/main/java/dev/braintrain/backend/job/Job.java
+++ back-end/src/main/java/dev/braintrain/backend/job/Job.java
@@ -1,14 +1,10 @@
 package dev.braintrain.backend.job;
-
 import dev.braintrain.backend.company.Company;
 import dev.braintrain.backend.crew.Crew;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 public class Job {
@@ -16,43 +12,32 @@ public class Job {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
-
     @Column(name = "job_name", nullable = false)
     private String name;
-
     @Column(name = "job_description", nullable = false)
     private String description;
-
     @Column(name = "job_status", nullable = false)
     private String status;
-
     @ManyToOne
     @JoinColumn(name = "crew_id")
     private Crew crew;
-
     @Column(name = "created_date", nullable = true, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
-
     @Column(name = "updated_date", nullable = true)
     @UpdateTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     private Date updatedDate;
-
     @ManyToOne
     @JoinColumn(name = "company_id", nullable = true, columnDefinition = "BIGINT DEFAULT NULL")
     private Company company;
-
     @Column(name = "start_date", nullable = true)
     private LocalDate startDate;
-
     @Column(name = "end_date", nullable = true)
     private LocalDate endDate;
-
     @Lob
     @Column(name = "file_data", nullable = true)
     private byte[] fileData;
-
     public Job(String name, String description, String status, Crew crew, Company company, LocalDate startDate, LocalDate endDate, byte[] fileData) {
         this.name = name;
         this.description = description;
@@ -63,7 +48,6 @@ public class Job {
         this.endDate = endDate;
         this.fileData = fileData;
     }
-
     public Job(String name, String description, String status, Crew crew, Company company) {
         this.name = name;
         this.description = description;
@@ -71,104 +55,80 @@ public class Job {
         this.crew = crew;
         this.company = company;
     }
-
     public Job(String name, String description, String status, Company company) {
         this.name = name;
         this.description = description;
         this.status = status;
         this.company = company;
     }
-
     public Job() {
-
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
     public String getStatus() {
         return status;
     }
-
     public void setStatus(String status) {
         this.status = status;
     }
-
     public Crew getCrew() {
         return crew;
     }
-
     public void setCrew(Crew crew) {
         this.crew = crew;
     }
-
     public Date getCreatedDate() {
         return createdDate;
     }
-
     public void setCreatedDate(Date createdDate) {
         this.createdDate = createdDate;
     }
-
     public Date getUpdatedDate() {
         return updatedDate;
     }
-
     public void setUpdatedDate(Date updatedDate) {
         this.updatedDate = updatedDate;
     }
     public Company getCompany() {
         return company;
     }
-
     public void setCompany(Company company) {
         this.company = company;
     }
     public LocalDate getStartDate() {
         return startDate;
     }
-
     public void setStartDate(LocalDate startDate) {
         this.startDate = startDate;
     }
-
     public LocalDate getEndDate() {
         return endDate;
     }
-
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
     }
-
     public byte[] getFileData() {
         return fileData;
     }
-
     public void setFileData(byte[] fileData) {
         this.fileData = fileData;
     }
-
     @PrePersist
     protected void onCreate() {
         if (createdDate == null) {
